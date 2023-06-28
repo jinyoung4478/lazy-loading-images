15b2aedc6fcc1187475ffc9012b251f89582c502
Harry <skswls0@daum.net>
2023-06-28 17:59:19 +0900

    이미지 나열 및 기초 CSS 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import img1 from '/imgs/img1.png';
+import img2 from '/imgs/img2.jpg';
+import img3 from '/imgs/img3.jpg';
+import img4 from '/imgs/img4.jpg';
+import img5 from '/imgs/img5.jpg';
+import img6 from '/imgs/img6.jpg';
+import img7 from '/imgs/img7.jpg';
+import img8 from '/imgs/img8.jpg';
+import img9 from '/imgs/img9.jpg';
+import img10 from '/imgs/img10.jpg';
+import img11 from '/imgs/img11.jpg';
+import img12 from '/imgs/img12.jpg';
+import img13 from '/imgs/img13.jpg';
+import img14 from '/imgs/img14.jpg';
+import img15 from '/imgs/img15.jpg';
+import img16 from '/imgs/img16.jpg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <body>
+      <div className="grid">
+        <img src={img1} />
+        <img src={img2} />
+        <img src={img3} />
+        <img src={img4} />
+        <img src={img5} />
+        <img src={img6} />
+        <img src={img7} />
+        <img src={img8} />
+        <img src={img9} />
+        <img src={img10} />
+        <img src={img11} />
+        <img src={img12} />
+        <img src={img13} />
+        <img src={img14} />
+        <img src={img15} />
+        <img src={img16} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </body>
+  );
 }
 
-export default App
+export default App;
+
+//
+//           <img src={reactLogo} className="logo react" alt="React logo" />
