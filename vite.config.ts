b33032b02b605fb63be37d1eecc870da7aa2746e
김진영 <skswls0@daum.net>
2023-06-28 23:33:41 +0900

    gh-pages 배포를 위한 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/lazy-loading-images',
   plugins: [react()],
   resolve: {
-    alias: [{find: '@', replacement: '/src'}],
+    alias: [{ find: '@', replacement: '/src' }],
+  },
+  build: {
+    chunkSizeWarningLimit: 100000000,
   },
 });
